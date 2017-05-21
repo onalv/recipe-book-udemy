2c6b1f8ac195bdecd9530cebe0dca6f39a2cb9fd
navao <omarnavaalvarez@gmail.com>
2017-05-21 00:02:56 -0500

    Shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,49 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+
 import {Recipe} from "../recipe";
+import {ShoppingListService} from "../../shopping-list/shopping-list.service";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  selectedRecipe: Recipe;
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-
-  constructor() { }
+  constructor(private sls: ShoppingListService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private recipesService: RecipeService){ }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+      }
+    );
+  }
+
+  onAddToShoppingList() {
+    this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  onEdit() {
+    this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+  }
+
+  onDelete() {
+    this.recipesService.deleteRecipe(this.selectedRecipe);
+    this.router.navigate(['/recipes']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,18 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from "./recipe";
 
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html'
 })
-export class RecipesComponent {
-
-  selectedRecipe: Recipe;
-
-  constructor() { }
-
-  ngOnInit() {
-
-  }
-
-}
+export class RecipesComponent {}
